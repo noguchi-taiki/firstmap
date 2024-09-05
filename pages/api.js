704ae83c5762b05c47279bef8a2c8f397198fd63
noguchi-taiki <taiki.13n1015@gmail.com>
2024-09-06 00:58:29 +0900

    非同期のため正しく処理されないことがあったからそこの改善

--- pages/api.js
+++ pages/api.js
@@ -2,18 +2,22 @@ import { useEffect, useState } from "react";
 import styles from "../styles/Layout.module.css"
 
 export default function App(){
-    const [data,setData] = useState('');
+    const [data,setData] = useState([]);
 
     useEffect(()=>{
         fetch('http://127.0.0.1:8000/api')
             .then((res) => res.json())
-            .then((res) => setData(res))
+            .then((data) => setData(data))
             .catch((error) => console.error('Error:', error));
     }, []);
 
     return (
         <>
-            <h1 className={styles.api}>{data[0].name}</h1>
+            {data.length > 0 ? (
+                <h1 className={styles.api}>{data[0].name}</h1>
+            ) : (
+                <h1>Loading...</h1>
+            )}
         </>
     )
 }
